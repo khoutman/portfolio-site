f71f07dc67f67d00896f3c62cb147255938df89a
KyleHoutman-ICF <kyle.houtman@icfnext.com>
2022-02-17 18:39:19 -0600

    trying to fix providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,18 +2,29 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.56.0"
+      version = ">= 3.20.0"
     }
+
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      version = "3.1.0"
     }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "3.1.0"
+    }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.8.0"
+      version = ">= 2.0.1"
     }
   }
-
 
   cloud {
     organization = "khoutman"
@@ -22,6 +33,8 @@ terraform {
       name = "portfolio-site-workflow"
     }
   }
+
+  required_version = ">= 0.14"
 }
 
 
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,12 +1,7 @@
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "portfolio-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
+  cluster_name = "portfolio-eks"
 }
 
 module "vpc" {
