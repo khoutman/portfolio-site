c130b69e01010fd0edf5fa378f2d5863947b716b
KyleHoutman-ICF <kyle.houtman@icfnext.com>
2022-02-19 14:05:43 -0600

    fix local variables

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,15 +5,6 @@ variable "region" {
 
 data "aws_availability_zones" "available" {}
 
-locals {
-  cluster_name = "portfolio-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
-}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
