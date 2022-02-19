bb715899cac15c692bb0c4dcff337ccd73030e0c
KyleHoutman-ICF <kyle.houtman@icfnext.com>
2022-02-19 11:35:10 -0600

    fix formatting

--- terraform/alb-ingress.tf
+++ terraform/alb-ingress.tf
@@ -1,11 +1,11 @@
 module "alb_controller" {
-  source  = "git::https://github.com/GSA/terraform-kubernetes-aws-load-balancer-controller?ref=v4.3.0gsa"
 
   providers = {
     kubernetes = "kubernetes.eks",
     helm       = "helm.eks"
   }
 
+  source           = "git::https://github.com/GSA/terraform-kubernetes-aws-load-balancer-controller?ref=v4.3.0gsa"
   k8s_cluster_type = "eks"
   k8s_namespace    = "kube-system"
 
