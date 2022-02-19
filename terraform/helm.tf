ecfc50093b07f7640eb93944f036b0ed54e57958
KyleHoutman-ICF <kyle.houtman@icfnext.com>
2022-02-19 12:56:41 -0600

    remove old files

--- terraform/helm.tf
+++
@@ -1,8 +0,0 @@
-provider "helm" {
-  alias = "eks"
-  kubernetes {
-    host                   = data.aws_eks_cluster.target.endpoint
-    token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.target.certificate_authority[0].data)
-  }
-}
--- terraform/vars.tf
+++
@@ -1,3 +0,0 @@
-variable "alb_controller_depends_on" {
-  description = "Resources that the module should wait for before starting the controller. For example if there is no node_group, 'aws_eks_fargate_profile.default'"
-}
